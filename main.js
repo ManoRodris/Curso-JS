825d9eacc32e3d182a276fe0149bb31c6df74b3b
Rodrigo Dórea <rodrigodoreafelix@gmail.com>
2024-09-05 12:57:46 -0300

    Iniciando curso em JS

---
+++ main.js
@@ -0,0 +1,18 @@
+//Tipos de dados em JS
+
+const message = "Hello world!";
+
+let number_test = 12;
+
+const boolean_var = true
+
+let array_example = ["Rodrigo", "Dórea", "Felix", 22] ;
+
+/*
+if (boolean_var === true) {
+    console.log(array_example)
+}
+*/
+
+//console.log("Meu nome é " + array_example[0] + " " + array_example[1] + ", " + array_example[3] + " anos")
+console.log(`Meu nome é ${array_example[0]} ${array_example[1]}, tenho ${array_example[3]} anos`)
